7334b06b6b8070bb5c538c1f6d5aae69d0ac80e6
Sofiane Farkhani <farkhans@3il.fr>
2021-09-22 21:21:34 +0200

    feat: add vertical menu

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,5 +1,6 @@
 import java.awt.Dimension;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,6 +9,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class Main {
 
@@ -20,9 +23,13 @@ public class Main {
     private JMenuBar menuBar;
     private JMenu menu;
     private JMenuItem close, about;
+    private JButton loadBtn, delBtn, moveBtn;
+    private JPanel verticalMenu;
 
     public Main() {
         frame= new JFrame("TP1");
+
+        verticalMenu = new JPanel(new GridLayout(3,1));
 
         // MENUBAR
         menuBar=new JMenuBar();  
@@ -32,6 +39,14 @@ public class Main {
         menu.add(close); menu.add(about);
         menuBar.add(menu);
 
+        loadBtn = new JButton("Charger une image");
+        delBtn = new JButton("Supprimer l'image");
+        moveBtn = new JButton("Déplacer l'image");
+
+        verticalMenu.add(loadBtn);
+        verticalMenu.add(delBtn);
+        verticalMenu.add(moveBtn);
+
         frame.setJMenuBar(menuBar);  
         frame.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
         frame.setSize(FRAME_WIDTH,FRAME_HEIGHT);  
@@ -39,6 +54,10 @@ public class Main {
         frame.setVisible(true); 
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
+
+        frame.add(verticalMenu, BorderLayout.LINE_START);
+        frame.pack();
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         close.addActionListener(new ActionListener() {
