44ff3a4015d8f24823d937b8862f6a2cc487ef82
Sofiane Farkhani <farkhans@3il.fr>
2021-10-08 11:19:21 +0200

    feat: add TP4

---
+++ TP4/desktop/src/com/interfacesgraphiques3d/tp4/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.interfacesgraphiques3d.tp4.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.interfacesgraphiques3d.tp4.Drop;
+
+
+public class DesktopLauncher {
+	
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Drop(), config);
+	}
+}
